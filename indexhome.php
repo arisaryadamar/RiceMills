a3b7cb35a62dac487396918c51c193cd06f98a17
Dewiratih27 <58056731+Dewiratih27@users.noreply.github.com>
2020-01-13 08:44:14 +0700

    Add files via upload

---
+++ indexhome.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>TAKIS.</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+  <!-- navbar -->
+  <nav class="navbar navbar-default navbar-static-top">
+    <div class="container" id="navbar">
+      <div class="collapse navbar-collapse" id="navcol-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li role="presentation"><a href="#home">HOME</a></li>
+          <li class="dropdown"><a href="#">VIEW</a>
+          <ul class="isi-dropdown">
+            <li><a href="#">Data Pemasokan </a></li>
+            <li><a href="#">Data Penggilingan </a></li>
+            <li><a href="#">Grafik Pemasokan </a></li>
+            <li><a href="#">Grafik Penjualan </a></li>
+          </ul>
+        </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- jumbotron -->
+  <div id="background">
+    <div class="jumbotron">
+      <h1>RICE MILLS</h1>
+      </div>
+  </div>
+  <!-- jumbotron -->
+
+  <!-- container atas -->
+  <div id="home">
+    <div class="isi">
+      <div class="container atas">
+        <h1>Main Menu</h1>
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+            <i href="#" class="row-menu"><img src="assets/img/masok.png" width="100" height="100"></i>
+            <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+            <h3>PEMASOKAN</h3>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+            <i href="#" class="row-menu"><img src="assets/img/giling.png" width="100" height="100"></i>
+            <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+            <h3>PENGGILINGAN</h3>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+            <i href="#" class="row-menu"><img src="assets/img/jual.png" width="100" height="100"></i>
+            <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+            <h3>PENJUALAN</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- container atas -->
+
+ 
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
